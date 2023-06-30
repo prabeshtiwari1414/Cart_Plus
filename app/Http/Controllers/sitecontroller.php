516290a873cea60eea6c5162d30cc311289d0e9f
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-06-30 16:48:42 +0545

    setup bootstrap, fontawesome, jquery

---
+++ app/Http/Controllers/sitecontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function getHome()
+    {
+        return view('site.template');
+    }
+}
---
+++ resources/views/site/template.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Cart Plus</title>
+        <link rel="icon" href="{{ asset('style/image/cartplus.png') }}" />
+        <link
+            rel="stylesheet"
+            href="{{ asset('style/fontawersome/all.css') }}"
+        />
+        <link rel="stylesheet" href="{{asset('style/bootstrap/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{ asset('style/css/style.css') }}">
+    </head>
+    <body>
+        Lorem ipsum dolor sit.
+
+
+        <button onclick="getAlert()" class="btn btn-outline-success"><i class="fa-solid fa-house"></i></button>
+
+        <script src ="{{asset('style/js/jquery.js')}}"></script>
+        <script src="{{ asset('style/bootstrap/bootstrap.js') }}"></script>
+
+<script src="{{ asset('style/fontawersome/all.js') }}"></script>
+
+<script src="{{ asset('style/js/script.js') }}"></script>
+    </body>
+</html>
