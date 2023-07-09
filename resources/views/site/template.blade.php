53572bc1d88681564657d7d8be58e54ba7620a9c
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-07-09 13:59:33 +0545

    fotter

--- resources/views/site/template.blade.php
+++ resources/views/site/template.blade.php
@@ -13,19 +13,36 @@
         <link rel="stylesheet" href="{{ asset('style/css/style.css') }}">
     </head>
     <body>
-      <section class="header">
-      <nav class="upper_nav d-flex ">
-        <div class="upper_nav_contains d-flex justify-content-between">
-        <div class="location "><span><i class="fa-solid fa-location-dot fa-flip"></i>Pokhara-8,Srijana Chowk</span></div>
-        <div class="tele "><span><i class="fa-solid fa-phone fa-bounce"></i>+977 061-538358</span></div>
-        <div class="icons d-flex ">
-          <div class="facebook icon"><span><i class="fa-brands fa-facebook" ></i></span></div>
-          <div class="instagram icon"><span><i class="fa-brands fa-instagram" ></i></span></div>
-          <div class="linkedin icon"><span><i class="fa-brands fa-linkedin" ></i></span></div>
-          <div class="cart icon"><span><i class="fa-solid fa-cart-shopping" ></i></span></div>
+        <div class="container bg-success text-light">
+          <div class="row">
+            <div class="col-9">
+              <div class="row">
+                <div class="col-4 ">
+                  <div class="location "><span><i class="fa-solid fa-location-dot fa-flip"></i>Pokhara-8,Srijana Chowk</span></div>
+                  
+                </div>
+                <div class="col-3 t">
+                  <div class="tele "><span><i class="fa-solid fa-phone fa-bounce"></i>+977 061-538358</span></div>
+
+                </div>
+              </div>
+            </div>
+            <div class="col-3 ">
+              <div class="row">
+                <div class="col-2"></div>
+                <div class="col-10">
+                <div class="icons d-flex ">
+                  <div class="facebook icon"><span><i class="fa-brands fa-facebook text-light"  ></i></span></div>
+                  <div class="instagram icon"><span><i class="fa-brands fa-instagram  text-light"  ></i></span></div>
+                  <div class="linkedin icon"><span><i class="fa-brands fa-linkedin  text-light"  ></i></span></div>
+                  <div class="cart icon"><span><i class="fa-solid fa-cart-shopping  text-light"  ></i></span></div>
+              </div>
+                </div>
         </div>
+            </div>
+          </div>
         </div>
-      </nav>
+
     <nav class="navbar navbar-expand-lg bg-body-tertiary ">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="{{ asset('style/image/cartplus.png') }}" alt="Logo" height="100px"></a>
@@ -36,7 +53,6 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
               <a class="nav-link " href="{{ route('user.home') }}">Home </a>
-              
               </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('user.about') }}">About</a>
@@ -46,7 +62,6 @@
               Gallery
             </a>
         </li>
-       
         </ul>
          <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -59,7 +74,206 @@
       <section class="second">
         @yield('middlesection')
       </section>
-      <section class="third">footer</section>
+      <section class="third" >
+        <!-- Remove the container if you want to extend the Footer to full width. -->
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #3e4551"
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!--Grid column-->
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">FOOTER CONTENT</h5>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae modi cum ipsam ad, illo possimus laborum ut
+              reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque?
+              Reiciendis assumenda iusto sapiente inventore animi?
+            </p>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 4</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 4</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 4</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Link 4</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="mb-4" />
+
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Register for free</span>
+          <a href="{{ route('register') }}"><button type="button" class="btn btn-outline-light btn-rounded">
+            Sign up!
+          </button></a>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+
+      <hr class="mb-4" />
+
+      <!-- Section: Social media -->
+      <section class="mb-4 text-center">
+        <!-- Facebook -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-facebook-f"></i
+          ></a>
+
+        <!-- Twitter -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-twitter"></i
+          ></a>
+
+        <!-- Google -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-google"></i
+          ></a>
+
+        <!-- Instagram -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-instagram"></i
+          ></a>
+
+        <!-- Linkedin -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-linkedin-in"></i
+          ></a>
+
+        <!-- Github -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="fab fa-github"></i
+          ></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2023 Copyright:
+      <a class="text-white" href="#"
+         >prabeshhtiwari.com.np</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+<!-- End of .container -->
+      </section>
         <script src ="{{asset('style/js/jquery.js')}}"></script>
         <script src="{{ asset('style/bootstrap/bootstrap.js') }}"></script>
 <script src="{{ asset('style/fontawersome/all.js') }}"></script>
